a7f0f9b366d1c6324be94dcbcc309ff0e307b94e
data-fixed <dataskul78@proton.me>
2024-08-02 16:12:46 -0700

    Create gtlcheat.js

---
+++ a1-gtlcheat1/version 2/source/gtlcheat.js
@@ -0,0 +1,3 @@
+window.alert("dont run or use this bookmarklet if it is not from https://github.com/data-fixed/bookmarklets/tree/main thanks...");
+window.alert(title_data);
+show_alert("cheat by https://github.com/data-fixed", "purple")
