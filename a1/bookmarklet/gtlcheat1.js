ab6d3924c1547a4f416ada0dbec2ca9bc02cef73
data-fixed <dataskul78@proton.me>
2024-08-02 15:49:22 -0700

    Create gtlcheat1.js

---
+++ a1/bookmarklet/gtlcheat1.js
@@ -0,0 +1,3 @@
+javascript:(function()%7Banswer_input.value %3D title_data%3B%0Ashow_alert("cheat by https%3A%2F%2Fgithub.com%2Fdata-fixed"%2C "purple")%3B%7()%3BD)
+// made with https://caiorss.github.io/bookmarklet-maker/
+// for https://sachinjha1822.github.io/Guess-The-logo/
