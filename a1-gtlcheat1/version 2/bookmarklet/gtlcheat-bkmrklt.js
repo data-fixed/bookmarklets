3358e1875b1ff4488161c8a41d341982f199e484
data-fixed <dataskul78@proton.me>
2024-08-02 16:15:57 -0700

    Update gtlcheat-bkmrklt.js

--- a1-gtlcheat1/version 2/bookmarklet/gtlcheat-bkmrklt.js
+++ a1-gtlcheat1/version 2/bookmarklet/gtlcheat-bkmrklt.js
@@ -1,1 +1,5 @@
-
+javascript:(function()%7Bwindow.alert(%22dont%20run%20or%20use%20this%20bookmarklet%20if%20it%20is%20not%20from%20https%3A%2F%2Fgithub.com%2Fdata-fixed%2Fbookmarklets%2Ftree%2Fmain%20thanks...%22)%3B%0Awindow.alert(title_data)%3B%0Ashow_alert(%22cheat%20by%20https%3A%2F%2Fgithub.com%2Fdata-fixed%22%2C%20%22purple%22)%7D)()%3B
+// thanks for using
+// made with https://caiorss.github.io/bookmarklet-maker/
+// made for https://sachinjha1822.github.io/Guess-The-logo/
+// the games source code: https://github.com/sachinjha1822/Guess-The-logo/tree/main
