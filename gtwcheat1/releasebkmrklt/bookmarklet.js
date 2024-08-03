98dcb13d52029817c956965aff12cca8901ba83b
data-fixed <dataskul78@proton.me>
2024-08-02 17:13:20 -0700

    Create bookmarklet.js

---
+++ gtwcheat1/releasebkmrklt/bookmarklet.js
@@ -0,0 +1,4 @@
+javascript:(function()%7Bwindow.alert(%22dont%20run%20or%20use%20this%20bookmarklet%20if%20it%20is%20not%20from%20https%3A%2F%2Fgithub.com%2Fdata-fixed%2Fbookmarklets%2Ftree%2Fmain%20thanks...%22)%3B%0Amessage.innerText%20%3D%20word%7D)()%3B
+// thanks for using!
+//read licence before using
+// and read rules before using
